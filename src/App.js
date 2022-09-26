6f930a264c4a33ce24bfaae9fdfccc8466e832df
sujon-chambugong <sujonchambugong@gmail.com>
2022-09-26 14:51:22 +0600

    connected with github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// 2. UI/UX Link-   https://www.figma.com/file/46GH4iF125pb5xtf4qnvi3/Webpage-Design?node-id=0%3A1
+// 4. Resources Link-   https://drive.google.com/file/d/1KB8o4Cnsi11IR4r7_EnWJJu-Q4DrAK5C/view?usp=sharing
+// Angrybaaz Service Private Limited
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-xl font-bold'>Angrybaaz Service Private Limited</h1>
+    </div>
+  );
+}
+
+export default App;
